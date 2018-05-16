0a761fceea530008af0b9c97bde4e309ea50a0df
Ashwini kumar sinha <31278395+ASHWINISINHA@users.noreply.github.com>
2018-05-16 11:34:13 +0530

    Update control.ino

--- control.ino
+++ control.ino
@@ -1,3 +1,27 @@
+
+MIT License
+
+Copyright (c) 2018 Ashwini kumar sinha
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
 int x=0;
 int y=0;
 int sen1=2;
@@ -35,7 +59,7 @@ void loop() {
     
     duration=pulseIn (echopin,HIGH);
     
-    distance= duration/58.2; // Distance is found using this formula 
+    distance= duration/58.2; 
     delay (50);
 x=digitalRead(sen1);
 y=digitalRead(sen2);
